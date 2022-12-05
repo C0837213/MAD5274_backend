de1bac4648227556b0381a415fb63ab3a2b350d2
mark chan <mark_chan74@hotmail.com>
2022-12-05 15:01:01 -0500

    ✅ update login logic

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,22 @@
 import express from "express";
 import mongoClient from "../helper/mongoClient.js";
-import { createUser, getAllUsers } from "../services/users.js";
+import { createUser, getAllUsers, getUser } from "../services/users.js";
 
 const router = express.Router();
 
-/* GET users listing. */
+router.post("/login", async function (req, res) {
+  const loginForm = req.body;
+  const user = await getUser(loginForm.email);
+  console.log(user);
+  if (!user) {
+    res.json({ status: "success", msg: "user not found" });
+  } else if (user && loginForm.password !== user.password) {
+    res.json({ status: "success", msg: "password is not correct" });
+  } else if (user && loginForm.password === user.password) {
+    res.json({ status: "success", user });
+  }
+});
+
 router.get("/", async function (req, res) {
   const result = await getAllUsers();
   res.json(result);
