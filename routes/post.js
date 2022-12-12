d03e91eea137f3a1af890e121a8a4a782e48f988
Philip Chau <kyoc2020@gmail.com>
2022-12-12 00:04:36 -0500

    added update post api

--- routes/post.js
+++ routes/post.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import mongoClient from '../helper/mongoClient.js';
+import { ObjectId } from 'mongodb';
 
 const router = express.Router();
 const postCollection = await mongoClient().db('MAD5274').collection('post')
 
 const getAllPost = async () => {
   let result
-  await postCollection.find({}).toArray()
+  await postCollection.find({status:{$ne: 'completed'}}).toArray()
     .then(data => {
       result = data
     })
@@ -29,6 +30,18 @@ router.post('/', async function(req, res) {
       console.log(err)
     })
   res.json(result)
+});
+
+router.put('/', async function(req, res) {
+  req.body._id = ObjectId(req.body._id)
+  await postCollection.replaceOne({ _id: ObjectId(req.body._id) }, req.body)
+    .then(
+      res.status(200).json("Success")
+    )
+    .then(e=>console.log(e))
+    .catch(err => {
+      console.log(err)
+    })
 });
 
 export default router
